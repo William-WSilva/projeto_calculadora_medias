bbf238c9c0975cd3dec20d69d4aadea5a9bfdadf
William Silva <williamreis.wsilva@gmail.com>
2023-06-13 12:39:42 -0300

    Limitando as casas decimais da média final

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ function calculaMediaFinal() {
 function atualizarMediaFinal() {
     const mediaFinal = calculaMediaFinal();
 
-    document.getElementById("media-final-valor").innerHTML = mediaFinal;
+    document.getElementById("media-final-valor").innerHTML = mediaFinal.toFixed(2);
     document.getElementById("media-final-resultado").innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 
 
